e0a05f2615d9c91d957584bce65a620925f42261
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-18 10:39:11 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Dfrw
+admin.site.register(Dfrw)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Dfrw(models.Model):
+    'Generated Model'
+    cncm = models.BigIntegerField()
+    irore = models.BigIntegerField()
